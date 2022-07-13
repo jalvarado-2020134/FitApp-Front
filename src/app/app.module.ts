2810a3dce107266e3ada7a9c70ba950ee1e056cd
Josue Alvarado <jalvarado-2020134@kinal.edu.gt>
2022-07-13 09:14:27 -0600

    Se agregó componente Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Type } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent} from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { UserRestService } from 'src/app/services/user-rest.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+    token: any;
+    role: any;
+    identity: any;
 
-  constructor() { }
+  constructor(
+    private userRest: UserRestService, private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.token = this.userRest.getToken();
+    this.identity = this.userRest.getIdentity();
+    this.role = this.userRest.getIdentity().role;
   }
+
+  logOut(){
+    localStorage.clear();
+    Swal.fire({
+      icon: 'success',
+      title: 'Bye,' + '  ' + 'Good Luck'
+    })
+    this.router.navigateByUrl('/login');
+  }
+
 
 }
